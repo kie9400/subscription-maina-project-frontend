bd39227a46705bf92505126fbce0d3c1bc404267
hogeun <sw8865@naver.com>
2025-04-20 20:15:28 +0900

    메인 페이지(비회원) 임시 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Footer from './components/Footer';
 import MainPage from './pages/MainPage';
+import Header from './components/Header';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="app">
+      <Header />    
+      <hr className="line" />
       <MainPage />
       <Footer />
     </div>
