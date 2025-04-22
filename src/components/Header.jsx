37143564d90a3ef351aa95ea6cf302a7d4a4bfde
kie9400 <sw8865@naver.com>
2025-04-22 12:23:08 +0900

    관리자, 사용자 로그인시 헤더 변경 반영

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../styles/Header.module.css';
 import Logo from '../assets/images/logo.png';
+import { useAuth } from '../context/AuthContext';
 
 const Header = () => {
+  const { isLoggedIn, isAdmin, logout } = useAuth();
+
   return (
     <header className={styles.header}>
       <div className={styles.headerWrapper}>
@@ -11,8 +14,31 @@ const Header = () => {
           <img src={Logo} className={styles.logo} alt="구독매니아 로고" />
         </Link>
         <nav className={styles.navLinks}>
-          <Link to="/login" className={styles.navLink}>로그인</Link>
-          <Link to="/signup" className={styles.navLink}>회원가입</Link>
+          {isLoggedIn ? (
+            <>
+              <Link to="/" onClick={logout} className={styles.navLink}>
+                로그아웃
+              </Link>
+              {isAdmin ? (
+                <Link to="/admin" className={styles.navLink}>
+                  관리자페이지
+                </Link>
+              ) : (
+                <Link to="/mypage" className={styles.navLink}>
+                  마이페이지
+                </Link>
+              )}
+            </>
+          ) : (
+            <>
+              <Link to="/login" className={styles.navLink}>
+                로그인
+              </Link>
+              <Link to="/signup" className={styles.navLink}>
+                회원가입
+              </Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
